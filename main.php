dca2feb9956f0bd504fabaaf45b94713800a43c0
Amin Vakil <info@aminvakil.com>
2020-04-03 21:35:08 +0430

    Sends dice on every input

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL);
+//error_reporting(E_ALL);
 require_once 'token.php';
 $website = "https://api.telegram.org/bot" . $botToken;
 $update = file_get_contents("php://input");
@@ -11,9 +11,12 @@ function sendMessage($chatId, $message) {
     $url = $GLOBALS[website]."/sendmessage?chat_id=".$chatId."&text=".urlencode($message);
     file_get_contents($url);
 }
+function sendDice($chatId) {
+    $url = $GLOBALS[website]."/sendDice?chat_id=".$chatId;
+    file_get_contents($url);
+}
 set_time_limit(0);
-$folder = "";
 ini_set('max_execution_time', 0);
-sendMessage ($chatId, "Let's start");
-//sendMessage ($chatId, var_dump($updateArray));
+//sendMessage ($chatId, $updateArray["message"]["text"]);
+sendDice ($chatId);
 ?>
