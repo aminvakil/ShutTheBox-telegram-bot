f66ba1708aaf3dfbc750583c2f2dbbd7917f529b
Amin Vakil <info@aminvakil.com>
2020-04-03 21:41:24 +0430

    Replies dice to input

--- main.php
+++ main.php
@@ -6,17 +6,19 @@ $update = file_get_contents("php://input");
 $updateArray = json_decode($update, TRUE);
 $chatId = $updateArray["message"]["chat"]["id"];
 $message = $updateArray["message"]["text"];
+$message_id = $updateArray["message"]["message_id"];
 $id = $updateArray["message"]["from"]["id"];
 function sendMessage($chatId, $message) {
     $url = $GLOBALS[website]."/sendmessage?chat_id=".$chatId."&text=".urlencode($message);
     file_get_contents($url);
 }
-function sendDice($chatId) {
-    $url = $GLOBALS[website]."/sendDice?chat_id=".$chatId;
+function sendDice($chatId,$message_id) {
+    $url = $GLOBALS[website]."/sendDice?chat_id=".$chatId."&reply_to_message_id=".$message_id;
     file_get_contents($url);
 }
 set_time_limit(0);
 ini_set('max_execution_time', 0);
 //sendMessage ($chatId, $updateArray["message"]["text"]);
-sendDice ($chatId);
+//sendMessage ($chatId, $message_id);
+sendDice ($chatId,$message_id);
 ?>
